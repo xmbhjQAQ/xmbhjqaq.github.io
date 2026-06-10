e012c7897e05559ab1507e272b1c21c6e8cc768c
xmbhjQAQ <2719989194@qq.com>
2026-06-11 02:22:48 +0800

    Site updated: 2026-06-11 02:22:47

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/06/11/site-online/"];function toRandomPost(){
+var posts=["2026/06/11/site-online/","2026/06/11/测试文章/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
